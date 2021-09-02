03bb860094e5f678377d9f79b4e27e5f61143835
ahmed.hamma <ahmed.hamma@le-campus-numerique.fr>
2021-09-02 16:42:50 +0200

    fin itération 5

--- src/main/java/com/example/projetThymeleaf/PersonnageController/PersonnageController.java
+++ src/main/java/com/example/projetThymeleaf/PersonnageController/PersonnageController.java
@@ -52,12 +52,24 @@ public class PersonnageController {
         model.addAttribute("personnage",personnage);
         return "ajoutPerso";
     }
+
     @RequestMapping(value = {"/AjoutPerso"}, method = RequestMethod.POST)
     public String Ajoutform(Model model, @ModelAttribute Personnage personnage){
         restTemplate.postForObject("http://localhost:8080/Perso", personnage, String.class);
         return "redirect:/ListePerso";
     }
+    @RequestMapping(value = "/UpdatePerso", method = RequestMethod.POST)
+    public String updatePerso(@ModelAttribute Personnage personnage) {
+        restTemplate.put("http://localhost:8080/Perso", personnage);
+        return "redirect:/ListePerso";
+    }
+    @GetMapping("/UpdatePerso/{id}")
+    public String updatePerso(Model model, @PathVariable int id) {
+        Personnage persoDetail = restTemplate.getForObject("http://127.0.0.1:8080/Perso/" + id , Personnage.class);
+        model.addAttribute("updatePerso", persoDetail);
+        return "updatePerso";
 
+    }
 
 //    @RequestMapping(value = {"/AjoutPerso"}, method = RequestMethod.POST)
 //    public String savePersonnage(Model model,
